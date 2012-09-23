1e14b6d8ba5bccf7ecd51e613d6934906f2cf721
Stevie Mayhew <stevie@heyday.co.nz>
2012-09-24 09:43:40 +1200

    UPDATE: shipping error processing

--- code/Heystack/Subsystem/Shipping/Input/Processor.php
+++ code/Heystack/Subsystem/Shipping/Input/Processor.php
@@ -8,7 +8,7 @@
 /**
  * Input namespace
  */
-namespace Heystack\Subsystem\Shipping\Input;
+namespace Ecoya;
 
 use Heystack\Subsystem\Core\Input\ProcessorInterface;
 
@@ -24,7 +24,7 @@ use Heystack\Subsystem\Shipping\Interfaces\ShippingHandlerInterface;
  * @author Glenn Bautista <glenn@heyday.co.nz>
  * @package Ecommerce-Shipping
  */
-class Processor implements ProcessorInterface
+class ShippingInputProcessor implements ProcessorInterface
 {
 
     /**
@@ -59,10 +59,14 @@ class Processor implements ProcessorInterface
      */
     public function process(\SS_HTTPRequest $request)
     {
+        
         $data = $request->requestVars();
 
         $shippingFields = $this->shippingService->getDynamicMethods();
 
+        
+        // populate defaults
+        
         foreach ($data as $key => $value) {
 
             if (in_array($key, $shippingFields)) {
@@ -72,10 +76,148 @@ class Processor implements ProcessorInterface
             }
 
         }
+        
+        // errors
+        $errors = array();
+        
+        {
+            
+            if (!isset($data['BillingFirstName']) || !$data['BillingFirstName']) {
+                
+                $errors['BillingFirstName_Error'] = array('Error' => 'Please enter a first name.');
+                
+            }   
+
+            if (!isset($data['BillingSurname']) || !$data['BillingSurname']) {
+                
+                $errors['BillingSurname_Error'] = array('Error' => 'Please enter a surname.');
+                
+            }   
+            
+            if (!isset($data['BillingEmail']) || !filter_var($data['BillingEmail'], FILTER_VALIDATE_EMAIL)) {
+                
+                $errors['BillingEmail_Error'] = array('Error' => 'Please enter an email address.');
+                
+            }   
+   
+            if (!isset($data['BillingAddressLine1']) || !$data['BillingAddressLine1']) {
+                
+                $errors['BillingAddressLine1_Error'] = array('Error' => 'Please enter an address.');
+                
+            }   
+            
+            if (!isset($data['BillingCity']) || !$data['BillingAddressLine1']) {
+                
+                $errors['BillingCity_Error'] = array('Error' => 'Please enter a city.');
+                
+            }  
+            
+            if (!isset($data['BillingPostcode']) || !$data['BillingPostcode']) {
+                
+                $errors['BillingPostcode_Error'] = array('Error' => 'Please enter a postcode.');
+                
+            }   
+            
+            if (!isset($data['BillingCountry']) || !$data['BillingCountry']) {
+                
+                $errors['BillingCountry_Error'] = array('Error' => 'Please select a country.');
+                
+            }   
+
+        }
+        
+        // if the delivery is billing, populate those fields that we can
+        if ($data['delivery'] == 'billing') {
+            
+            $this->shippingService->BillingAsShipping = true;
+            $this->shippingService->BillingFirstName = $data['BillingFirstName'];
+            $this->shippingService->BillingSurname = $data['BillingSurname'];
+            $this->shippingService->BillingEmail = $data['BillingEmail'];
+            
+            $this->shippingService->FirstName = $data['BillingFirstName'];
+            $this->shippingService->Surname = $data['BillingSurname'];
+            $this->shippingService->Email = $data['BillingEmail'];
+            
+            $this->shippingService->AddressLine1 = $data['BillingAddressLine1'];
+            $this->shippingService->AddressLine2 = $data['BillingAddressLine2'];
+            $this->shippingService->City = $data['BillingCity'];
+            $this->shippingService->Postcode = $data['BillingPostcode'];
+            $this->shippingService->Country = $data['BillingCountry'];
+
+
+        } else {
+            
+            //errors
+            {
+            
+                if (!isset($data['FirstName']) || !$data['FirstName']) {
+
+                    $errors['FirstName_Error'] = array('Error' => 'Please enter a first name.');
+
+                }   
+
+                if (!isset($data['Surname']) || !$data['Surname']) {
+
+                    $errors['Surname_Error'] = array('Error' => 'Please enter a surname.');
+
+                }   
+
+                if (!isset($data['Email']) || !filter_var($data['Email'], FILTER_VALIDATE_EMAIL)) {
+
+                    $errors['Email_Error'] = array('Error' => 'Please enter an email address.');
+
+                }   
+
+                if (!isset($data['AddressLine1']) || !$data['AddressLine1']) {
+
+                    $errors['AddressLine1_Error'] = array('Error' => 'Please enter an address.');
+
+                }   
+
+                if (!isset($data['City']) || !$data['City']) {
+
+                    $errors['City_Error'] = array('Error' => 'Please enter a city.');
+
+                }  
+
+                if (!isset($data['Postcode']) || !$data['Postcode']) {
+
+                    $errors['Postcode_Error'] = array('Error' => 'Please enter a postcode.');
+
+                }   
+
+                if (!isset($data['Country']) || !$data['Country']) {
+
+                    $errors['Country_Error'] = array('Error' => 'Please select a country.');
+
+                }   
+
+            }
+            
+            $this->shippingService->BillingAsShipping = false;
+            $this->shippingService->FirstName = $data['BillingFirstName'];
+            $this->shippingService->Surname = $data['BillingSurname'];
+            $this->shippingService->Email = $data['BillingEmail'];
+            
+        }
+        
+        if (count($errors)) {
+            $errors = 
+                    
+                    array(
+                        'success' => false,
+                        'Errors' => $errors
+                    
+            );
+
+            return array_merge($errors, $data);
+        }
 
         $this->shippingService->saveState();
 
-        return array('success' => 'true');
+        return array(
+            'success' => true
+        );
     }
 
 }
